08a7966968b2abe0de42c424c1ae4620edec5392
Bilal Ashraf <ba2961289@gmail.com>
2019-10-31 01:14:33 +0500

    Node 1

---
+++ cNode.cpp
@@ -0,0 +1,22 @@
+//self referential node class i.e. stack elements
+#include <iostream>
+using namespace std;
+
+class Node
+{
+		int n;						//keep track of no. of nodes added
+		int data;					//data carrier
+	public:
+		Node *next;					//pointer to refer the next node
+	Node(){ this->n = 0;this->data = -1; next = NULL;} //default class constructor
+	
+	Node(int d, int num){ this->data = d; this->n = num;}			//parameterized constructor
+	
+	void set_data(int v, int v1){ this->data = v; this->n = v1;} //setter function
+	
+	int get_data(){ return data;}   //getter function
+	int get_n() { return n;}
+	void print(){ cout << data;}	//prints data
+		
+};
+#pragma once
